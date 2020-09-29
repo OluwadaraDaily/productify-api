4c92f49a513c3d292efa887fbdda547c55dbad83
oluwadara <daraoloye99@gmail.com>
2020-09-29 20:42:09 +0100

    Initial commit

--- config/database.js
+++ config/database.js
@@ -5,6 +5,9 @@ const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
+const Url = require('url-parse')
+
+const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
 
 module.exports = {
   /*
@@ -16,8 +19,14 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'mysql'),
-
+  // connection: Env.get('DB_CONNECTION', 'mysql'),
+  connection: {
+    host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+    port: Env.get('DB_PORT', ''),
+    user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+    password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+    database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+  } 
   /*
   |--------------------------------------------------------------------------
   | Sqlite
