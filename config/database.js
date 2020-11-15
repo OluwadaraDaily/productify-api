5c078e86987da462bc2f835d3733001cf65a1a7b
oluwadara <daraoloye99@gmail.com>
2020-11-15 19:48:44 +0100

    Added DATABASE_URL of the Heroku Postgres

--- config/database.js
+++ config/database.js
@@ -19,7 +19,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'mysql'),
+  connection: Env.get('DB_CONNECTION', 'pg'),
   // connection: {
   //   host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
   //   port: Env.get('DB_PORT', ''),
@@ -79,12 +79,13 @@ module.exports = {
   */
   pg: {
     client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+    connection: Env.get('DATABASE_URL', 'localhost'),
+    // connection: {
+      // host: Env.get('DB_HOST', 'localhost'),
+      // port: Env.get('DB_PORT', '5432'),
+      // user: Env.get('DB_USER', 'root'),
+      // password: Env.get('DB_PASSWORD', 'daraoloye99'),
+      // database: Env.get('DB_DATABASE', 'productify')
+    // }
   }
 }
